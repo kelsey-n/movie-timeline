7c604e4d39faebd22bc797d99e559854bad8b2e7
kelsey-n <kan317@nyu.edu>
2022-08-26 11:20:00 -0400

    update description

--- src/components/Description.svelte
+++ src/components/Description.svelte
@@ -1,16 +1,16 @@
 <div class="title">Wes Anderson: Many Hits, Few Misses<br /><br /></div>
 <div class="text">
   Wes Anderson's masterful eye for crafting a scene is evident in all of his
-  movies. His style has become instantly recognizable and has inspired admiring
+  movies. His style has become instantly recognizable and inspired admiring
   Instagram accounts, analyses and articles on his own inspirations. <br />
   <br />
-  This timeline of his movies shows the critical reception and financial reception
-  (budget and box office numbers) of each of his movies. In only two occasions has
-  his budget exceeded the box office amount - his first movie,
-  <em>Bottle Rocket</em>
-  and <em>The Life Aquatic with Steve Zissou</em>. <br />
+  This timeline of his movies shows each one's reception - both critical (Rotten
+  Tomatoes score) and financial (budget and box office numbers). In only two occasions
+  has his budget exceeded the box office amount reaped - his first movie,
+  <em>Bottle Rocket</em>, and <em>The Life Aquatic with Steve Zissou</em>.
+  <br />
   <br /> This ode to the director also celebrates his skillful use of color -
-  hover on a movie to see more!
+  hover on a movie to find out how!
   <br /> <br />
 </div>
 <div><br /></div>
